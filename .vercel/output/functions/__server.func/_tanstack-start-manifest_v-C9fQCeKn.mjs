53f68798156405ee896b8b4539b75bf694ff28ce
Ayomiposy <ayomiposi.joshua@preferreddigitalbusiness.com>
2026-06-26 08:31:23 +0100

    chore: stop tracking vercel build outputs

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-C9fQCeKn.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/ayomiposi/dev/Programming/My project/ogunseunjoshua/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-wEckDrVz.js"] }, "/": { filePath: "/home/ayomiposi/dev/Programming/My project/ogunseunjoshua/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DaRJU_q0.js"] } }, clientEntry: "/assets/index-wEckDrVz.js" });
-export {
-  tsrStartManifest
-};
